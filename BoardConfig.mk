0440a6d04a51068d239be30b45d68d6e7154ba26
McFy49 <g.matthieu49@gmail.com>
2018-05-24 18:50:35 +0000

    initial twrp device tree

---
+++ BoardConfig.mk
@@ -0,0 +1,54 @@
+#
+# Copyright (C) 2018 The TwrpBuilder Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/samsung/a3y17lte
+
+TARGET_BOARD_PLATFORM := exynos5
+TARGET_BOOTLOADER_BOARD_NAME := a3y17lte
+
+# Recovery
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 39845888 
+BOARD_FLASH_BLOCK_SIZE := 1000000
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_EXCLUDE_SUPERSU := true
+include $(LOCAL_PATH)/kernel.mk
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/a3y17lte/seEnforcing.mk
+TW_INCLUDE_CRYPTO := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a9
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+
+# TWRP
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+ifeq ($(TW_THEME),)
+TW_THEME := portrait_hdpi
+endif
+RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
