9bc8188e16a63cc67f4d72c396bead97157f95b5
McFy49 <g.matthieu49@gmail.com>
2018-06-28 16:05:21 +0200

    a3y17lte: Add Wi-Fi supplicant configs from stock

--- device.mk
+++ device.mk
@@ -48,3 +48,8 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf \
 
+# Wi-Fi Configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/vendor/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/vendor/etc/wifi/wpa_supplicant_overlay.conf
+
