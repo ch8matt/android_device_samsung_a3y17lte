d07e352b1a4f5ed369a951e372fa7e44b7d9e57e
McFy49 <g.matthieu49@gmail.com>
2018-06-28 17:30:00 +0200

    a3y17lte: Add audio configs from stock

--- device.mk
+++ device.mk
@@ -63,3 +63,14 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/qcom_cfg.ini:system/etc/firmware/wlan/qcom_cfg.ini \
     $(LOCAL_PATH)/configs/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/WCNSS_cfg.dat
 
+
+###########
+#  Audio  #
+###########
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/audio/mixer_gains.xml:system/etc/mixer_gains.xml \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
