69a0e8e31503b797d898fc0b6b16ba7125849b20
McFy49 <g.matthieu49@gmail.com>
2018-06-28 15:11:43 +0200

    a3y17lte: Initialise skeleton tree

---
+++ device.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/samsung/a3y17lte
+
+# Inherit from Exynos7870-common
+$(call inherit-product, device/samsung/exynos7870-common/device-common.mk)
+
+# call the proprietary setup
+$(call inherit-product, vendor/samsung/a3y17lte/a3y17lte-vendor.mk)
